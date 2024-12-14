093942b8434f0f402a43d2a4a38c813d117bc7ba
yungan9 <geyun105@naver.com>
2024-12-14 17:07:30 +0900

    feat: 카카오맵에 마커 여러개 표시

---
+++ frontend/src/data/mapdata.js
@@ -0,0 +1,10 @@
+export const mapdata = [
+  {
+    title: "버터핑거 팬케익스 분당점",
+    latlng: { lat: 37.372208, lng: 127.105593 },
+  },
+  {
+    title: "이마트 에브리데이 분당정자점",
+    latlng: { lat: 37.371887, lng: 127.106263 },
+  },
+];
--- frontend/src/pages/App/App_main.jsx
+++ frontend/src/pages/App/App_main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { mapdata } from "../../data/mapdata";
 
 export default function App_main() {
   const center = { lat: 37.371421, lng: 127.104744 };
+
   return (
     <>
       <Container>
@@ -11,7 +13,22 @@ export default function App_main() {
           center={center}
           style={{ width: "390px", height: "100vh" }}
           level={2}
-        />
+        >
+          {mapdata.map((data) => (
+            <MapMarker
+              key={`${data.title}-${data.latlng}`}
+              position={data.latlng}
+              image={{
+                src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
+                size: {
+                  width: 24,
+                  height: 35,
+                },
+              }}
+              title={data.title}
+            />
+          ))}
+        </Map>
       </Container>
     </>
   );
