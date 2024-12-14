d3a99f516bc2f792ac2d2ae9ba70db6690134a96
yungan9 <geyun105@naver.com>
2024-12-14 20:42:24 +0900

    feat: 가게 리스트 컴포넌트

--- frontend/src/components/bottomsheet/Content.jsx
+++ frontend/src/components/bottomsheet/Content.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import StoreList from "../StoreList/StoreList";
 
 export default function Content() {
-  return <div>Content</div>;
+  return (
+    <>
+      <Container>
+        <StoreList
+          name="가게 이름이 너무 길어요;;;;;;"
+          phone="010-0000-0000"
+          state="요청 가능"
+          location="위치위치위치"
+        />
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  margin: 24px;
+`;
