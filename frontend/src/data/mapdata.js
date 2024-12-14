9cc93ef5bbf133326b23a796e568d6f41c255d70
yungan9 <geyun105@naver.com>
2024-12-15 03:38:16 +0900

    chore: 테스트를 위한 더미데이터 수정

--- frontend/src/data/mapdata.js
+++ frontend/src/data/mapdata.js
@@ -2,9 +2,22 @@ export const mapdata = [
   {
     title: "버터핑거 팬케익스 분당점",
     latlng: { lat: 37.372208, lng: 127.105593 },
+    state: "요청 불가",
+    phone: "010-0102-9876",
+    location: "150m · 경기도 성남시 분당구",
+  },
+  {
+    title: "이마트 에브리데이 분당정자",
+    latlng: { lat: 37.371887, lng: 127.106263 },
+    state: "요청 가능",
+    phone: "010-0231-6789",
+    location: "150m · 경기도 성남시 분당구",
   },
   {
     title: "이마트 에브리데이 분당정자점",
     latlng: { lat: 37.371887, lng: 127.106263 },
+    state: "요청 가능",
+    phone: "010-0231-6876",
+    location: "150m · 경기도 성남시 분당구",
   },
 ];
