e1ed492f2bee09866df26217a2c126edabbe034e
yungan9 <geyun105@naver.com>
2024-12-14 21:13:56 +0900

    design : 가이드 버튼 추가

--- frontend/src/pages/App/App_main.jsx
+++ frontend/src/pages/App/App_main.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { mapdata } from "../../data/mapdata";
 import BottomSheet from "../../components/bottomsheet/BottomSheet";
+import Guide from "../../assets/guide.svg";
 
 export default function App_main() {
   const center = { lat: 37.371421, lng: 127.104744 };
@@ -10,6 +11,11 @@ export default function App_main() {
   return (
     <>
       <Container>
+        <GuideBtn>
+          <GuideIcon src={Guide} />
+          가이드
+        </GuideBtn>
+
         <Map
           center={center}
           style={{ width: "390px", height: "100vh" }}
@@ -44,4 +50,28 @@ const Container = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   overflow: auto;
   justify-content: center;
+`;
+
+const GuideBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  width: 98px;
+  height: 40px;
+  background-color: #36aa78;
+  border-radius: 20px;
+  font-size: 16px;
+  font-weight: 700;
+  color: white;
+  z-index: 1000;
+  cursor: pointer;
+`;
+
+const GuideIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 8px;
 `;
